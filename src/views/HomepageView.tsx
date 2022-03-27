e353b1fb1a73f1736703cd3017b5152c0bfd230c
Laura Marina Bianchi <lauramarinabianchi@gmail.com>
2022-03-27 19:25:09 +0200

    add jest and step on feature.yml with yarn test

--- src/views/HomepageView.tsx
+++ src/views/HomepageView.tsx
@@ -1,24 +1,19 @@
-import type { NextPage } from "next";
-import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { withPrefix } from "../../with-prefix";
-import { Main } from "../components/Main";
 
-const Home: NextPage = () => {
+type Props = {};
+export const HomepageView: React.FC<Props> = () => {
+  const router = useRouter();
   return (
-    <Main>
+    <>
       <h1 style={{ fontSize: "3rem", display: "flex", alignItems: "center", gap: 24 }}>
         Weather with blob
         <span>
           <img src={withPrefix("/images/blob-cookie.png")} alt="blob cookie" width={50} height={50} />
         </span>
       </h1>
-
-      <Link href="/today" passHref>
-        <a>Che tempo farà domani?</a>
-      </Link>
-    </Main>
+      <button onClick={() => router.push("/today")}>Che tempo farà domani?</button>
+    </>
   );
 };
-
-export default Home;
