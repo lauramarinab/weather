0f5da7e16570b3d16938be82b7b20ebc2e69e5d9
lauramarinab <38502071+lauramarinab@users.noreply.github.com>
2022-03-27 17:31:08 +0000

    Deploying to gh-pages from @ lauramarinab/weather@67455ae55519ceee125b295daf773226db8930df 🚀

--- _next/static/xR2vRatjBLVfBTamz76QB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7a795a1770f1ebc2.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],"/today":["static/chunks/pages/today-ab060714222713ae.js"],sortedPages:["/","/_app","/_error","/today"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
