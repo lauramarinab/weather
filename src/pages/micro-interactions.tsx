77f68e72d697036a18087b2bc5f93541183b9bda
Laura Marina Bianchi <lauramarinabianchi@gmail.com>
2022-03-31 22:16:45 +0200

    add prev commit

--- src/pages/micro-interactions.tsx
+++
@@ -1,7 +0,0 @@
-import { MicroInteractionsView } from "../views/MicroInteractionsView";
-
-const MicroAnimations: React.FC = () => {
-  return <MicroInteractionsView />;
-};
-
-export default MicroAnimations;
